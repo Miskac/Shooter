17a59a88ce3eef117e90dd485a2ea8d61bfc90a5
Miskac <marko.miskov@gmail.com>
2021-10-27 17:12:05 +0200

    Movement

--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -19,6 +19,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Called for forwards/backwards input
+	void MoveForward(float Value);
+
+	// Called for right/left input
+	void MoveRight(float Value);
+
+	// Called via input to turn at a given rate
+	// @param Rate This is a normlised rate, i.e. 1.0 means 100% of desired turn rate
+	void TurnAtRate(float Rate);
+
+	// Called via input to look up/down at a given rate
+	// @param Rate This is a normlised rate, i.e. 1.0 means 100% of desired turn rate
+	void LookUpAtRate(float Rate);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -34,6 +48,14 @@ private:
 	// Camera that follows the Character
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
+	// Base turn rate in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	// Base look up/down rate in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
 
 public:
 	// Returns CameraBoom Subobject
